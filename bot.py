dc77430e9c7586a8c73e595927fcec23ba1ee213
helpmyanmar1000-star <helpmyanmar1000@gmail.com>
2026-01-10 15:42:21 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,13 +21,13 @@ def keep_alive():
 
 # --- ၂။ Bot Setup ---
 # သင့်ရဲ့ Token ကို ဒီမှာထည့်ပါ (သို့မဟုတ် Render Settings ထဲမှာ ထည့်ပါ)
-TOKEN = "8577050959:AAGG3qQ71Hhm-26FhjN-DkKVcRhVVAUHEFw" 
+TOKEN = "8577050959:AAHGB2kDNHJBvOhU0Vk3AMIITX-frckqfXM" 
 bot = telebot.TeleBot(TOKEN)
 
 # @username သုံးရင် မျက်တောင်အဖွင့်အပိတ် " " ပါရပါမယ်
-FROM_CHAT = "@mghlamyo555"  # ချန်နယ်အဟောင်း username
-TO_CHAT = "@mghlamyo666"    # ချန်နယ်အသစ် username
-START_ID = 1519               # စမယ့် message id
+FROM_CHAT = "@mgkyawgyi555"  # ချန်နယ်အဟောင်း username
+TO_CHAT = "@mgkyawgyi666"    # ချန်နယ်အသစ် username
+START_ID = 988               # စမယ့် message id
 
 def start_forwarding():
     video_count = 0        # အပုဒ် ၁၀၀ စစ်ဖို့
@@ -41,28 +41,22 @@ def start_forwarding():
             # ဗီဒီယိုကို ကူးယူခြင်း
             bot.copy_message(TO_CHAT, FROM_CHAT, current_id)
             
-            video_count += 1
-            total_batch_count += 1
+            video_count += 100
+            total_batch_count += 100
             print(f"ID {current_id} ကို ပို့ပြီးပါပြီ။ (Batch: {total_batch_count})")
 
-            # တစ်ပုဒ်ချင်းစီကြား Random Delay (အကောင့်မပိတ်အောင် 3 စက္ကန့်မှ 5 စက္ကန့်)
-            time.sleep(random.randint(3, 5))
+            # တစ်ပုဒ်ချင်းစီကြား Random Delay (အကောင့်မပိတ်အောင် 3 စက္ကန့်မှ 4 စက္ကန့်)
+            time.sleep(random.randint(5, 6))
 
-            # ၅ မိနစ်တစ်ခါ ဗီဒီယို ၁၀၀ ပို့မယ်
-            if video_count >= 100:
-                print("ဗီဒီယို ၁၀၀ ပြည့်လို့ ၅ မိနစ် ခေတ္တနားနေပါတယ်...")
-                time.sleep(300) 
-                video_count = 0
-
-            # ၁၀၀၀ ပြည့်ရင် ၁ နာရီနားမယ်
-            if total_batch_count >= 1000:
-                print("ဗီဒီယို ၁၀၀၀ ပြည့်လို့ ၁ နာရီ အကြာကြီးနားပါမယ်...")
-                time.sleep(3600)
+            # ၂၀၀၀ ပြည့်ရင် နာရီဝက် နားမယ်
+            if total_batch_count >= 2000:
+                print("ဗီဒီယို ၂၀၀၀ ပြည့်လို့ နာရီဝက် အကြာကြီးနားနေပါသည်...")
+                time.sleep(1800)
                 total_batch_count = 0
 
         except Exception as e:
             # Message မရှိရင် (သို့) Video မဟုတ်ရင် ၅ စက္ကန့်နားပြီး နောက်တစ်ခုသွားမယ်
-            time.sleep(5)
+            time.sleep(3)
             pass
         
         current_id += 1
